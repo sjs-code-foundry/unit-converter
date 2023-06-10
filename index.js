c626f3ae03b6879c32255bab9ffe1192945befc9
sjs-code-foundry <bds.stephen@gmail.com>
2023-06-10 19:06:57 +0100

    2023/06/10 19:00

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
+// Get Elements
+inputConv = document.getElementById("input-convert")
+inputNum = document.getElementById("input-field")
+
+// Conversion Functions
+const lenFactor = 3.281 // ft in 1 meter
+const volFactor = 0.264 // gallons in 1 liter
+const massFactor = 2.204 // pounds in 1 kilo
+
+inputConv.addEventListener("click", function() {
+    console.log(`Button Clicked: ${inputNum.value} converted.`)
+})
