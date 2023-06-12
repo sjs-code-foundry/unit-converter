650a680d47670a27ff135ade4a65ef4fa2d5bb93
sjs-code-foundry <bds.stephen@gmail.com>
2023-06-12 13:52:53 +0100

    2023/06/12 14:00

--- index.js
+++ index.js
@@ -1,12 +1,51 @@
 // Get Elements
 inputConv = document.getElementById("input-convert")
 inputNum = document.getElementById("input-field")
+dispLength = document.getElementById("disp-length")
+dispVolume = document.getElementById("disp-volume")
+dispMass = document.getElementById("disp-mass")
 
 // Conversion Functions
-const lenFactor = 3.281 // ft in 1 meter
-const volFactor = 0.264 // gallons in 1 liter
-const massFactor = 2.204 // pounds in 1 kilo
+const units = [["Meters","Feet"], ["Liters","Gallons"], ["Kilos","Pounds"]]
+const factors = [3.281, 0.264, 2.204] // ft/meter, gallons/liter, pounds/kilo
+const elements = [dispLength, dispVolume, dispMass]
 
 inputConv.addEventListener("click", function() {
-    console.log(`Button Clicked: ${inputNum.value} converted.`)
+    for (let i = 0; i < units.length; i++) {
+        const text = display(inputNum.value, factors[i], units[i])
+        storeUpload(text, units[i])
+        elements[i].textContent = text
+    }
 })
+
+function convert(amount, factor, flip) {
+    if (flip === true) {
+        return (amount / factor).toFixed(3)
+    } else {
+        return (amount * factor).toFixed(3)
+    }
+}
+
+function display(input, conv, units) {
+    return `${input} ${units[0]} = ${convert(input, conv, false)} ${units[1]} | ${input} ${units[1]} = ${convert(input, conv, true)} ${units[0]}`
+}
+
+function storeUpload(input, units) {
+    JSON.stringify(input)
+    localStorage.setItem(`convert${units[0]}${units[1]}`, JSON.stringify(input))
+}
+
+function storeRetrieve(units) {
+    let data = localStorage.getItem(`convert${units[0]}${units[1]}`)
+    if (data === null) {
+        return `0 ${units[0]} = 0 ${units[1]} | 0 ${units[1]} = 0 ${units[0]}`
+    } else {
+        return JSON.parse(data)
+    }
+
+}
+
+// Retrieve from localStorage
+for (let i = 0; i < units.length; i++) {
+    elements[i].textContent = storeRetrieve(units[i])
+}
